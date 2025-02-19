838d9c9af52f59c64ea09f0c335a47fbbb45edd1
matheus <matheus.pereira@zup.com.br>
2025-02-19 14:15:43 -0300

    feat: add class GlobalExceptionHandler with generic exception

---
+++ src/main/java/com/zup/CataLib/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.zup.CataLib.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Um erro inesperado aconteceu: " + ex.getMessage());
+    }
+}
