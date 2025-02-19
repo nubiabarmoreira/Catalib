5243af31fd939b29b7d8c5d54943c5291e97ca0e
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-19 17:28:17 -0300

    feat(controller):Implemented PUT endpoint to update an existing book by ID

--- src/main/java/com/zup/CataLib/controllers/BookController.java
+++ src/main/java/com/zup/CataLib/controllers/BookController.java
@@ -2,15 +2,12 @@ package com.zup.CataLib.controllers;
 
 import com.zup.CataLib.dtos.BookRequestDTO;
 import com.zup.CataLib.dtos.BookResponseDTO;
+import com.zup.CataLib.models.BookEntity;
 import com.zup.CataLib.services.BookService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/livros")
@@ -20,9 +17,15 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping
-    public ResponseEntity<BookResponseDTO> registerBookController(@Valid @RequestBody BookRequestDTO newBook) {
+    public ResponseEntity<BookResponseDTO> registerBookController(@RequestBody BookRequestDTO newBook) {
         BookResponseDTO saveNewBook = bookService.registerBookService(newBook);
         return ResponseEntity.status(HttpStatus.CREATED).body(saveNewBook);
+    }
 
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookEntity> updateBook(@PathVariable Long id, @RequestBody BookRequestDTO bookRequestDTO) {
+        BookEntity updateBook = bookService.updateBook(id, bookRequestDTO);
+        return ResponseEntity.ok(updateBook);
     }
 }
