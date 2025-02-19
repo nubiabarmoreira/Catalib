8bfd20cdd4683367fb293df7c5306ccc6a3e7416
matheus <matheus.pereira@zup.com.br>
2025-02-19 15:31:52 -0300

    feat: add delete in controller

--- src/main/java/com/zup/CataLib/controllers/BookController.java
+++ src/main/java/com/zup/CataLib/controllers/BookController.java
@@ -1,9 +1,20 @@
 package com.zup.CataLib.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.zup.CataLib.services.BookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/livros")
 public class BookController {
+
+    @Autowired
+    public BookServiceImpl bookService;
+
+    @DeleteMapping("/livros/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.ok().body("Produto deletado com sucesso!");
+    }
 }
