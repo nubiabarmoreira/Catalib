5781d7d49917cfb3effc50d5073c79b88a411f73
flaviahespanhol-zup <flavia.hespanhol@zup.com.br>
2025-02-19 14:11:40 -0300

    feat: extends JpaRepository in JPABookRepository

--- src/main/java/com/zup/CataLib/repositories/JPABookRepository.java
+++ src/main/java/com/zup/CataLib/repositories/JPABookRepository.java
@@ -1,4 +1,9 @@
 package com.zup.CataLib.repositories;
 
-public interface JPABookRepository {
+import com.zup.CataLib.models.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JPABookRepository extends JpaRepository<BookEntity, Long> {
 }
