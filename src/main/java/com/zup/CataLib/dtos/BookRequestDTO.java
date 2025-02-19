5d237c14797c71723a4489971d8e86d26506f0bf
flaviahespanhol-zup <flavia.hespanhol@zup.com.br>
2025-02-19 15:16:25 -0300

    feat: add validations on BookRequestDTO

--- src/main/java/com/zup/CataLib/dtos/BookRequestDTO.java
+++ src/main/java/com/zup/CataLib/dtos/BookRequestDTO.java
@@ -1,9 +1,27 @@
 package com.zup.CataLib.dtos;
 
 import com.zup.CataLib.models.BookEntity;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record BookRequestDTO(String title, String author, String category,
-                             int yearPublication, int stock, String description) {
+public record BookRequestDTO(
+        @NotEmpty(message = "O título não pode estar vazio")
+        @NotNull(message = "O título não pode ser nulo")
+        String title,
+        @NotEmpty(message = "O nome do autor não pode estar vazio")
+        @NotNull(message = "O nome do autor não pode ser nulo")
+        String author,
+        @NotEmpty(message = "A categoria pode estar vazia")
+        @NotNull(message = "A categoria não pode ser nula")
+        String category,
+        @NotEmpty(message = "O ano de publicação não pode estar vazio")
+        @NotNull(message = "O ano de publicação não pode ser nulo")
+        int yearPublication,
+        @NotNull(message = "O estoque não pode ser nulo")
+        @Min(value = 0, message = "O estoque deve ser um número inteiro maior ou igual a zero")
+        int stock,
+        String description) {
 
     public BookEntity dtoToEntity() {
         BookEntity newBook = new BookEntity();
