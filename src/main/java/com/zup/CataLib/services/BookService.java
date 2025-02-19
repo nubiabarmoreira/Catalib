f707bea99bcab611d603c8af3db33ce4607b7d43
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-19 15:21:18 -0300

    feat: add initial listAllBooks method in BookService

--- src/main/java/com/zup/CataLib/services/BookService.java
+++ src/main/java/com/zup/CataLib/services/BookService.java
@@ -1,5 +1,10 @@
 package com.zup.CataLib.services;
 
+import com.zup.CataLib.dtos.BookResponseDTO;
+
+import java.util.List;
+
 public interface BookService {
     void registerBookService();
+    List<BookResponseDTO> listAllBooks();
 }
--- src/main/java/com/zup/CataLib/services/BookServiceImpl.java
+++ src/main/java/com/zup/CataLib/services/BookServiceImpl.java
@@ -1,8 +1,13 @@
 package com.zup.CataLib.services;
 
+import com.zup.CataLib.dtos.BookResponseDTO;
+import com.zup.CataLib.models.BookEntity;
 import com.zup.CataLib.repositories.JPABookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -10,7 +15,19 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private JPABookRepository jpaBookRepository;
 
+    public BookServiceImpl(JPABookRepository jpaBookRepository){
+        this.jpaBookRepository = jpaBookRepository;
+    }
+
     public void registerBookService() {
 
+    }
+
+    @Override
+    public List<BookResponseDTO> listAllBooks() {
+        return jpaBookRepository.findAll()
+                .stream()
+                .map(BookResponseDTO::)
+                .collect(Collectors.toList());
     }
 }
