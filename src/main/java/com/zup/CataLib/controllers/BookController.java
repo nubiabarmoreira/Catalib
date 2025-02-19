87de253c88acda73f3737a1fe7ddfb6e0aad6afe
flaviahespanhol-zup <flavia.hespanhol@zup.com.br>
2025-02-19 15:05:33 -0300

    feat: add methods registerBook in BookServiceImpl and BookController

--- src/main/java/com/zup/CataLib/controllers/BookController.java
+++ src/main/java/com/zup/CataLib/controllers/BookController.java
@@ -1,9 +1,27 @@
 package com.zup.CataLib.controllers;
 
+import com.zup.CataLib.dtos.BookRequestDTO;
+import com.zup.CataLib.dtos.BookResponseDTO;
+import com.zup.CataLib.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/livros")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<BookResponseDTO> registerBookController(@RequestBody BookRequestDTO newBook) {
+        BookResponseDTO saveNewBook = bookService.registerBookService(newBook);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveNewBook);
+
+    }
 }
