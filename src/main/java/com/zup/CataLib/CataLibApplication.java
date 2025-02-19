63d01ccb7701b03d2be2e6116ae80de630cdc31d
RodolffoNMS <rodolffo.souza@zup.com.br>
2025-02-19 12:04:13 -0300

    chore: Add Spring initializ

---
+++ src/main/java/com/zup/CataLib/CataLibApplication.java
@@ -0,0 +1,13 @@
+package com.zup.CataLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CataLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CataLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/CataLib/CataLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.CataLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CataLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
