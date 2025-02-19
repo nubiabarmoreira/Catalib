7f2c157c095a1315e8b718abc2ebc24adcd24fe5
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-02-19 17:29:21 -0300

    feat(service): Added method to update an existing book by ID:

--- src/main/java/com/zup/CataLib/services/BookService.java
+++ src/main/java/com/zup/CataLib/services/BookService.java
@@ -2,7 +2,15 @@ package com.zup.CataLib.services;
 
 import com.zup.CataLib.dtos.BookRequestDTO;
 import com.zup.CataLib.dtos.BookResponseDTO;
+import java.util.List;
+import com.zup.CataLib.models.BookEntity;
 
 public interface BookService {
+    void deleteBook(Long id);
+
     BookResponseDTO registerBookService(BookRequestDTO newBook);
+    List<BookResponseDTO> listAllBooks();
+
+    //Método para atualizar livro existente
+    BookEntity updateBook (Long id, BookRequestDTO bookRequestDTO);
 }
