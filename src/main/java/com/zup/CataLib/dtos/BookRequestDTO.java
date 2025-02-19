872daf3efe19b342c6eda62142c8ba00c4dd7e11
flaviahespanhol-zup <flavia.hespanhol@zup.com.br>
2025-02-19 14:39:29 -0300

    feat: add BookResquestDTO

--- src/main/java/com/zup/CataLib/dtos/BookRequestDTO.java
+++ src/main/java/com/zup/CataLib/dtos/BookRequestDTO.java
@@ -1,4 +1,19 @@
 package com.zup.CataLib.dtos;
 
-public record BookRequestDTO(String title, String author, String category) {
+import com.zup.CataLib.models.BookEntity;
+
+public record BookRequestDTO(String title, String author, String category,
+                             int yearPublication, int stock, String description) {
+
+    public BookEntity dtoToEntity() {
+        BookEntity newBook = new BookEntity();
+        newBook.setTitle(title);
+        newBook.setAuthor(author);
+        newBook.setCategory(category);
+        newBook.setYearPublication(yearPublication);
+        newBook.setStock(stock);
+        newBook.setDescripton(description);
+
+        return newBook;
+    }
 }
